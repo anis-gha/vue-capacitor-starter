98cf64dab8980582bc7d8887adc3ecf69d697884
anis-gha <igodlik3.sales@gmail.com>
2024-10-08 23:09:13 +0200

    feat: added capacitor with android & ios support

---
+++ android/app/src/main/java/vue/capacitor/starter/MainActivity.java
@@ -0,0 +1,5 @@
+package vue.capacitor.starter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'vue.capacitor.starter',
+  appName: 'vue-capacitor-starter',
+  webDir: 'dist',
+}
+
+export default config
